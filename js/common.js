1954f57c8174a4d06d8666328b22366f89048f0a
newstreetpunk <newstreetpunk@yandex.ru>
2019-05-26 02:03:45 +0500

    HEADER desctop + JS

--- js/common.js
+++ js/common.js
@@ -1,80 +1,30 @@
-/*$(function() {
-
-	var header = $(".header"); // Меню
-	var scrollPrev = 0 // Предыдущее значение скролла
-	
-	$(window).scroll(function() {
-		var scrolled = $(window).scrollTop(); // Высота скролла в px
-		var firstScrollUp = false; // Параметр начала сколла вверх
-		var firstScrollDown = false; // Параметр начала сколла вниз
-		
-		// Если скроллим
-		if ( scrolled > 0 ) {
-			// Если текущее значение скролла > предыдущего, т.е. скроллим вниз
-			if ( scrolled > scrollPrev ) {
-				firstScrollUp = false; // Обнуляем параметр начала скролла вверх
-				// Если меню видно
-				if ( scrolled < header.height() + header.offset().top ) {
-					// Если только начали скроллить вниз
-					if ( firstScrollDown === false ) {
-						var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-						header.css({
-							"top": topPosition + "px"
-						});
-						firstScrollDown = true;
-					}
-					// Позиционируем меню абсолютно
-					header.css({
-						"position": "absolute"
-					});
-				// Если меню НЕ видно
-			} else {
-					// Позиционируем меню фиксированно вне экрана
-					header.css({
-						"position": "fixed",
-						"top": "-" + header.height() + "px"
-					});
-				}
-				
-			// Если текущее значение скролла < предыдущего, т.е. скроллим вверх
-		} else {
-				firstScrollDown = false; // Обнуляем параметр начала скролла вниз
-				// Если меню не видно
-				if ( scrolled > header.offset().top ) {
-					// Если только начали скроллить вверх
-					if ( firstScrollUp === false ) {
-						var topPosition = header.offset().top; // Фиксируем текущую позицию меню
-						header.css({
-							"top": topPosition + "px"
-						});
-						firstScrollUp = true;
-					}
-					// Позиционируем меню абсолютно
-					header.css({
-						"position": "absolute"
-						
-					});
-				} else {
-					// Убираем все стили
-					header.removeAttr("style");
-				}
-			}
-			// Присваеваем текущее значение скролла предыдущему
-			scrollPrev = scrolled;
-		}   
-	});
-
-});
-*/
-
+//Hide Menu on Scroll
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
+    document.getElementById("header").setAttribute(
+   "style", "top: 0; box-shadow: 0 3px 40px rgba(0,0,0,.1);");
   } else {
-    document.getElementById("header").style.top = "-150px";
+    document.getElementById("header").setAttribute(
+   "style", "top: -150px; box-shadow: 0 3px 30px transparent;");
   }
   prevScrollpos = currentScrollPos;
 }
+//
+
+
+
+$(function(){
+	//удаление атрибута 'style'
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100){
+			$('.header').attr('style')
+		}else{
+			$('.header').removeAttr('style');
+		}
+	});
+	//
+
+}, jQuery);

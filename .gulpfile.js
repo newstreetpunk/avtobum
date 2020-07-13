2c8d7061461bbe1cfb84da13e90e3ec531d7348b
Alexander Sabanov <alexsab@mail.ru>
2020-07-13 10:55:40 +0400

    upd gulpfile

--- .gulpfile.js
+++ .gulpfile.js
@@ -1,6 +1,7 @@
 		scripts: {
 			src: [
 				base.avtobum + '/libs/jquery/dist/jquery-3.3.1.min.js',
+				base.avtobum + '/libs/jquery.formstyler/jquery.formstyler.min.js',
 				'node_modules/slick-carousel/slick/slick.min.js',
 				base.avtobum + '/libs/lazyload/lazyload.js',
 				base.avtobum + '/js/common.js', // Always at the end

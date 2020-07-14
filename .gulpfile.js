d39fafca3b1a9a2bf0490114d38cd0ca52cc1c1a
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-07-14 12:55:39 +0500

    запустил

--- .gulpfile.js
+++ .gulpfile.js
@@ -22,11 +22,11 @@ let projects = {
 		scripts: {
 			src: [
 				base.avtobum + '/libs/jquery/dist/jquery-3.3.1.min.js',
-				// base.avtobum + '/libs/jquery.formstyler/jquery.formstyler.min.js',
+				base.avtobum + '/libs/jquery.formstyler/jquery.formstyler.min.js',
+				base.avtobum + '/libs/air-datepicker-master/js/datepicker.min.js',
+				base.avtobum + '/libs/select2/js/select2.min.js',
 				'node_modules/slick-carousel/slick/slick.min.js',
 				base.avtobum + '/libs/lazyload/lazyload.js',
-				base.avtobum + '/libs/air-datepicker-master/js/datepicker.js',
-				base.avtobum + '/libs/select2/js/select2.js',
 				base.avtobum + '/js/common.js', // Always at the end
 			],
 			dest:       base.avtobum + '/js',

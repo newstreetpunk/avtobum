f7823253f1b9f91102cf4055e3531d1a8e17354f
Alexander Sabanov <alexsab@mail.ru>
2021-08-09 22:16:13 +0400

    add nnov
    добавлен Нижний Новгород
    добавлена новая заглушка к 14 фев
    подсветка 8ой колонки

--- js/common.js
+++ js/common.js
@@ -3,10 +3,10 @@ var dataLayer = window.dataLayer || [];
 $(function() {
 
 	$( document ).ready(function() {
-		
+
 		//Hide Menu on Scroll
 		var hideHeaderPos = 100;
-		
+
 		/* опускающийся хедер при прокрутке вверх */
 		var prevScrollpos = window.pageYOffset;
 		$(window).scroll( function() {
@@ -114,7 +114,7 @@ $(function() {
 
 		/* добавление мобильной таблицы, если размер экрана меньше 767 */
 		var switched = false;
-		if (($(window).width() < 767) && !switched && document.getElementById('fixed-table-container_price')) { 
+		if (($(window).width() < 767) && !switched && document.getElementById('fixed-table-container_price')) {
 			switched = true;
 
 			var divs = document.querySelectorAll('#fixed-table-container_price'), i;
@@ -125,14 +125,14 @@ $(function() {
 			  fixTable(divs[i]);
 			}
 
-		} 
+		}
 
 		/* Разворачивание списка "Посмотреть подробную информация" о машине */
 		$('.toggle-more a').click(function(e) {
 			e.preventDefault();
-		  
+
 			var $this = $(this);
-		  
+
 			if ($this.parent().next().hasClass('show')) {
 				// hide current
 				$this.addClass('collapsed');
@@ -151,7 +151,7 @@ $(function() {
 				/**/
 
 				// open current
-				
+
 				if(typeof yaCounter28702351 !== 'undefined') yaCounter28702351.reachGoal('show_more_autoinfo');
 				$this.removeClass('collapsed');
 				$this.parent().next().toggleClass('show');
@@ -197,13 +197,13 @@ $(function() {
 
 
 			/* * /
-			$(selector).sort(function(a, b) { 
-				var aVal = parseInt($(a).data(dataName)); 
-				var bVal = parseInt($(b).data(dataName)); 
+			$(selector).sort(function(a, b) {
+				var aVal = parseInt($(a).data(dataName));
+				var bVal = parseInt($(b).data(dataName));
 				if(order == 'down')
-					return (aVal < bVal) ? -1 : (aVal > bVal) ? 1 : 0; 
+					return (aVal < bVal) ? -1 : (aVal > bVal) ? 1 : 0;
 				else
-					return (aVal < bVal) ? 1 : (aVal > bVal) ? -1 : 0; 
+					return (aVal < bVal) ? 1 : (aVal > bVal) ? -1 : 0;
 			}).appendTo($(selector).parent());
 			/* */
 
@@ -222,11 +222,11 @@ $(function() {
 		/* Действие кнопок сортировки автопарка */
 		$('.sort-order a').click(function(e) {
 			e.preventDefault();
-		  
+
 			var $this = $(this),
 				sort = $this.parent().data('sort'),
 				url = (location.hostname == 'localhost') ? 'arenda-mashin.html' : '/arenda-mashin/';
-		  
+
 			if ($this.parent().hasClass('sort-order_select')) {
 				$this.parent().toggleClass('sort-order_up');
 			} else {
@@ -237,7 +237,7 @@ $(function() {
 				$this.parent().addClass('sort-order_select');
 			}
 
-			
+
 			if($this.parent().hasClass('sort-order_up')) {
 				getSorted('.product', sort, 'up');
 				window.history.pushState({}, document.title, url+"?sort="+sort+"&order=up");
@@ -255,10 +255,10 @@ $(function() {
 				var query = window.location.search.substring(1);
 			}
 			// console.log("query : " + query);
-			
+
 			var params = window.location.search.substring(1);
 			// console.log("params : " + params);
-			
+
 			var vars = params.split("&");
 			// console.log("vars : " + vars);
 
@@ -268,13 +268,13 @@ $(function() {
 
 			for (var i = 0; i < vars.length; i++) {
 				var pair = vars[i].split("=");
-				
+
 				// console.log("pair : " + pair);
-				
-				if (pair[0] === 'sort' && sorts.indexOf(pair[1]) != -1) { 
+
+				if (pair[0] === 'sort' && sorts.indexOf(pair[1]) != -1) {
 					sort.push(pair[1]);
 				}
-				if (pair[0] === 'order' && orders.indexOf(pair[1]) != -1) { 
+				if (pair[0] === 'order' && orders.indexOf(pair[1]) != -1) {
 					sort.push(pair[1]);
 				}
 				if (pair[0] === what) {
@@ -329,10 +329,10 @@ $(function() {
 			}
 		  }
 		}
-		
+
 		/* если реальный сайт, то кнопка выбора города должна менять поддомен */
 		if(window.location.host.indexOf('avtobum63.ru') != -1) {
-			$("select.city").change(function(){                        
+			$("select.city").change(function(){
 				$(this).find("option:selected").each(function(){
 					//$('select').val($(this).attr("value"));
 					var url="//";
@@ -346,6 +346,9 @@ $(function() {
 					if($(this).attr("value")=="ufa"){
 						url += "ufa.avtobum63.ru";
 					}
+					if($(this).attr("value")=="nnov"){
+						url += "nnovgorod.avtobum63.ru";
+					}
 					url += window.location.pathname;
 					location.href = url;
 				});
@@ -353,7 +356,7 @@ $(function() {
 		}
 
 		// $( '.swipebox' ).swipebox();
-		
+
 		/* настройки галереи на детальной странице авто */
 		if($('[data-fancybox="group"]').length != 0)
 			$('[data-fancybox="group"]').fancybox({
@@ -405,7 +408,7 @@ $(function() {
 			/* * /
 			if(typeof yaCounter28702351 !== 'undefined') yaCounter28702351.reachGoal('open_venyoo_callclickhovermenu');
 			if($('#pv_head_wrapper').length) {
-				$('#pv_head_wrapper').click();        
+				$('#pv_head_wrapper').click();
 			}
 			if($('div[data-goal="callclickhovermenu"').length) {
 				$('div[data-goal="callclickhovermenu"').click();
@@ -472,7 +475,7 @@ $(function() {
 					break;
 				default:
 			}
-		}); 
+		});
 		$('a[data-class="calc"]').click(function(){
 			sendGoal('calcform_click','click calc btn');
 		});
@@ -536,7 +539,7 @@ $(function() {
 
 		// $('.calcForm select').styler();
 
-	
+
 	/* мобильная таблица прайслиста */
 	function fixTable(container) {
 		// Store references to table elements
@@ -674,10 +677,10 @@ $(function() {
 		};
 	}
 
-	/* плавная прокрутка страницы * /    
+	/* плавная прокрутка страницы * /
 	setTimeout(function() {
 		if (location.hash) {
-			// we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 
+			// we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116
 			window.scrollTo(0, 0);
 			target = location.hash.split('#');
 			smoothScrollTo($('#' + target[1]));
@@ -821,7 +824,7 @@ $(function() {
 			}, 1000);
 					resizePlayer(iframes, 16/9);
 			// if(window.innerWidth>window.innerHeight)
-			// else 
+			// else
 					// resizePlayer(iframes, 9/16);
 			// resizePlayer(iframes, window.innerWidth/window.innerHeight);
 		});
@@ -835,7 +838,7 @@ $(function() {
 		});
 		slideWrapper.on("lazyLoaded", function(event, slick, image, imageSource) {
 			// console.log([event, slick, image, imageSource]);
-			image.parent().css('background-image', 'url("' + imageSource + '")'); 
+			image.parent().css('background-image', 'url("' + imageSource + '")');
 			image.hide();
 			image.parent().addClass('show');
 			lazyCounter++;
@@ -865,14 +868,14 @@ $(function() {
 	});
 
 	// Resize event
-	$(window).on("resize.slickVideoPlayer", function(){  
+	$(window).on("resize.slickVideoPlayer", function(){
 		resizePlayer(iframes, 16/9);
 		// if(window.innerWidth>window.innerHeight)
-		// else 
+		// else
 		// resizePlayer(iframes, 9/16);
 		// resizePlayer(iframes, window.innerWidth/window.innerHeight);
 	});
-	
+
 
 	/* калькулятор начало */
 
@@ -894,7 +897,7 @@ $(function() {
 				}
 			//return placehold;
 			});
-		});	
+		});
 	}
 
 	placeholder();
@@ -911,13 +914,13 @@ $(function() {
 		$('#select-model').val(model);
 		$('#select-model').trigger('change');
 	}
-	
+
 	/* Select2 for Calc End */
 
 	// DAtaPicker
 	// Зададим стартовую дату
 	// почему-то если делать startDate зависимый от today, то timePicker работает не корректно
-	
+
 	var today = new Date(),
 		startDate = new Date(),
 		tomorrow = new Date(),
@@ -934,7 +937,7 @@ $(function() {
 
 		today.setMinutes(0);
 		today.setSeconds(0);
-		
+
 		startDate.setMinutes(0);
 		startDate.setSeconds(0);
 		startDate.setHours(today.getHours() + 1);
@@ -959,9 +962,9 @@ $(function() {
 		}).data('datepicker');
 		// startPicker.selectDate(startDate);
 	}
-	
+
 	if ($endPicker.length) {
-		
+
 		tomorrow.setMinutes(0);
 		tomorrow.setSeconds(0);
 		tomorrow.setDate(tomorrow.getDate() + 1);
@@ -987,7 +990,7 @@ $(function() {
 		}).data('datepicker');
 		// endPicker.selectDate(tomorrow);
 	}
-	
+
 
 	// Собираем данные формы -> открываем второе модальное окно (форму)
 	$('#form-calc').submit(function(){
@@ -1014,7 +1017,7 @@ $(function() {
 			textarea = parent.children('#'+ name +'_text'),
 			place = $this.data('place'),
 			price = $this.data('price');
-		
+
 		switch(id) {
 			case name+'_office':
 			case name+'_airport':
@@ -1069,47 +1072,47 @@ $(function() {
 				price += (100 * koff);
 			}
 		}
-		if(rentTime < 3) { 
+		if(rentTime < 3) {
 			// минимум 3 часа
 			price += pr_0[model]*3;
-		} 
-		else if(rentTime > 2 && rentTime < 9) { 
+		}
+		else if(rentTime > 2 && rentTime < 9) {
 			// Если аренда меньше 8 часов - то ставка почасовая (у каждой машины своя)
 			price += pr_0[model] * rentTime;
-		} 
-		else if(countDay == 1 || (rentTime > 8 && countDay == 0)) { 
+		}
+		else if(countDay == 1 || (rentTime > 8 && countDay == 0)) {
 			// Если аренда больше 8 часов - то это 1 сутки
 			price += pr_1[model];
 		}
-		else if(countDay > 1 && countDay < 4) { 
+		else if(countDay > 1 && countDay < 4) {
 			price += pr_2[model] * countDay;
 		}
-		else if(countDay > 3 && countDay < 7) { 
+		else if(countDay > 3 && countDay < 7) {
 			price += pr_3[model] * countDay;
 		}
-		else if(countDay > 6 && countDay < 16) { 
+		else if(countDay > 6 && countDay < 16) {
 			price += pr_4[model] * countDay;
 		}
-		else if(countDay > 15 && countDay < 31) { 
+		else if(countDay > 15 && countDay < 31) {
 			price += pr_5[model] * countDay;
 		}
-		else if(countDay > 30) { 
+		else if(countDay > 30) {
 			price += pr_week[model] * countDay;
 		}
 
 		if([6,7,20,21,22].includes(startPicker.lastSelectedDate.getHours())) {
 			price += 500;
-		} 
+		}
 		else if([23,0,1,2,3,4,5].includes(startPicker.lastSelectedDate.getHours())) {
 			price += 1000;
 		}
 		if([6,7,20,21,22].includes(endPicker.lastSelectedDate.getHours())) {
 			price += 500;
-		} 
+		}
 		else if([23,0,1,2,3,4,5].includes(endPicker.lastSelectedDate.getHours())) {
 			price += 1000;
 		}
-		
+
 		$('.dop-uslugi input[type=checkbox]:checked').each(function(index, el) {
 			var $this = $(this),
 				dopPrice = $this.data('pr'),
@@ -1137,7 +1140,7 @@ $(function() {
 
 			var str = $this.parent().find('label').text().trim();
 			if (str) dop_servs.push(str);
-			
+
 
 		});
 		if (dop_servs.length) {
@@ -1234,7 +1237,7 @@ $(function() {
 		// filter_car_list();
 	});
 	*/
-	
+
 	$('#sort_filter').on('change', function() {
 
 		let data = $('#sort_filter').serializeArray();
@@ -1246,16 +1249,16 @@ $(function() {
 			$('.utp').show();
 		} else {
 			let arr = [
-				{ name: "select-car-brand", data: 'brand', value: "" }, 
-				{ name: "select-body-type", data: 'type', value: "" }, 
-				{ name: "gearbox-at", data: 'gearbox', value: "1" }, 
-				{ name: "gearbox-mt", data: 'gearbox', value: "0" }, 
-				{ name: "towbar", data: 'farkop', value: "1" }, 
+				{ name: "select-car-brand", data: 'brand', value: "" },
+				{ name: "select-body-type", data: 'type', value: "" },
+				{ name: "gearbox-at", data: 'gearbox', value: "1" },
+				{ name: "gearbox-mt", data: 'gearbox', value: "0" },
+				{ name: "towbar", data: 'farkop', value: "1" },
 				{ name: "conditioner", data: 'conditioner', value: "нет" }
 			];
 
 			$.each(arr, function(index1, prop){
-				
+
 				$('.product').removeClass(prop);
 
 				let propArr = data.filter(function(elem) {
